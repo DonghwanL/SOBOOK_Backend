9aa1c5287cc03134c5ab12c57377ce9c01d74c38
Donghwan Lee <ehwan007@gmail.com>
2023-02-08 18:27:05 +0900

    🔧 TypeORM 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksModule } from './books/books.module';
 import { BookShelfModule } from './book-shelf/book-shelf.module';
 
 @Module({
-  imports: [BooksModule, BookShelfModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    }),
+    BooksModule,
+    BookShelfModule,
+  ],
 })
 export class AppModule {}
